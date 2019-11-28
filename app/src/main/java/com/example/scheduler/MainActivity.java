6f87ebd3b71ffa994fdfaf0ca092598f54ecb22f
dhruva4 <dhruva4@illinois.edu>
2019-11-28 13:20:01 -0500

    Added basic MainActivity UI and connected it to NewGameActivity

---
+++ app/src/main/java/com/example/scheduler/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.scheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addEvent = findViewById(R.id.addEvent);
+        addEvent.setOnClickListener(v -> {
+            startActivity(new Intent(this, NewGameActivity.class));
+            finish();
+        });    }
+}
---
+++ app/src/main/java/com/example/scheduler/NewGameActivity.java
@@ -0,0 +1,17 @@
+package com.example.scheduler;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public final class NewGameActivity extends AppCompatActivity {
+    @Override
+    @SuppressWarnings("ConstantConditions")
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game); // app/src/main/res/layout/activity_new_game.xml
+
+
+
+    }
+}
