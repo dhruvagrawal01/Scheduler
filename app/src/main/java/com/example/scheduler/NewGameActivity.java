c64edc29dc1d86fb1369104480700336e755ba7c
annmay sharma <annmays2@illinois.edu>
2019-12-02 15:47:27 -0600

    Added a textView to display the date selected on the calendar

--- app/src/main/java/com/example/scheduler/NewGameActivity.java
+++ app/src/main/java/com/example/scheduler/NewGameActivity.java
@@ -2,7 +2,9 @@ package com.example.scheduler;
 
 import android.os.Bundle;
 import android.widget.CalendarView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public final class NewGameActivity extends AppCompatActivity {
@@ -10,9 +12,17 @@ public final class NewGameActivity extends AppCompatActivity {
     @SuppressWarnings("ConstantConditions")
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_game); // app/src/main/res/layout/activity_new_game.xml
+        setContentView(R.layout.activity_new_game);
 
-        CalendarView calendar = findViewById(R.id.calendarView);
+        CalendarView calendar = (CalendarView) findViewById(R.id.calendarView);
+        TextView myDate = (TextView) findViewById(R.id.myDate);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = (month) + "/" + (dayOfMonth) + "/" + (year);
+                myDate.setText((date));
+            }
+        });
 
     }
 }
